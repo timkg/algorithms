2ed33be49cf07affbd544df3ad4f41ea6d8decf5
Tim Koch-Grunberg <tim.theodor@gmail.com>
2012-10-20 16:30:15 +0100

    added reverseSort, updated tests to include more corner cases

--- sorting/insertion.js
+++ sorting/insertion.js
@@ -13,6 +13,7 @@ define([], function(){
 			var currentIndex
 			  , currentValue
 			  , leftIndex
+			  , tempValue;
 			for(currentIndex = 1; currentIndex < len; currentIndex++) {
 				leftIndex = currentIndex - 1;
 				currentValue = inputArray[currentIndex];
@@ -24,7 +25,33 @@ define([], function(){
 			}
 			return inputArray;
 
+		},
+
+		reverseSort: function( inputArray ) {
+			if( Object.prototype.toString.call( inputArray ) != '[object Array]' ) {
+				throw new Error("insertion.sort() called on something that is not an array.");
+			}
+			var len = inputArray.length;
+			if(len < 2) {
+				return inputArray;
+			}
+			var currentIndex
+			  , currentValue
+			  , leftIndex
+			  , tempValue;
+			for(currentIndex = 1; currentIndex < len; currentIndex++) {
+				leftIndex = currentIndex - 1;
+				currentValue = inputArray[currentIndex];
+				while( leftIndex >= 0  && inputArray[leftIndex] < currentValue  ) {
+					inputArray[leftIndex+1] = inputArray[leftIndex];
+					leftIndex = --leftIndex;
+				}
+				inputArray[leftIndex+1] = currentValue;
+			}
+			return inputArray;
+
 		}
+
 	};
 
 	return insertion;
--- test/insertion.js
+++ test/insertion.js
@@ -13,7 +13,23 @@ describe("Insertion sort", function() {
 			assert.equal(typeof insertion.sort, "function");
 		})
 		it("should sort an array of numbers", function() {
-			assert.deepEqual([1, 2, 3, 3, 5, 6, 7, 8, 8, 8, 8, 9], insertion.sort([9,5,2,7,8,1,3,8,6,3,8,8]));
+			assert.deepEqual([9, 8, 8, 8, 8, 7, 6, 5, 3, 3, 2, 1], insertion.reverseSort([1,5,2,7,8,9,3,8,6,3,8,8]));
+			assert.deepEqual([9, 8, 8, 8, 8, 7, 6, 5, 3, 3, 2, 1], insertion.reverseSort([9,5,2,7,8,1,3,8,6,3,8,8]));
+			assert.deepEqual([9, 8, 8, 8, 8, 7, 6, 5, 3, 3, 2, 1], insertion.reverseSort([8,5,2,7,8,9,3,1,6,3,8,8]));
+			assert.deepEqual([9, 8, 8, 8, 8, 7, 6, 5, 3, 3, 2, 1], insertion.reverseSort([8,5,2,7,8,9,3,8,6,3,8,1]));
+			assert.deepEqual([9, 8, 8, 8, 8, 7, 6, 5, 3, 3, 2, 1], insertion.reverseSort([8,5,2,7,8,8,3,1,6,3,8,9]));
+		})
+	})
+	describe("#reverseSort()", function() {
+		it("should define a reverseSort() method", function() {
+			assert.equal(typeof insertion.reverseSort, "function");
+		})
+		it("should sort an array of numbers in decreasing order", function() {
+			assert.deepEqual([9, 8, 8, 8, 8, 7, 6, 5, 3, 3, 2, 1], insertion.reverseSort([1,5,2,7,8,9,3,8,6,3,8,8]));
+			assert.deepEqual([9, 8, 8, 8, 8, 7, 6, 5, 3, 3, 2, 1], insertion.reverseSort([9,5,2,7,8,1,3,8,6,3,8,8]));
+			assert.deepEqual([9, 8, 8, 8, 8, 7, 6, 5, 3, 3, 2, 1], insertion.reverseSort([8,5,2,7,8,9,3,1,6,3,8,8]));
+			assert.deepEqual([9, 8, 8, 8, 8, 7, 6, 5, 3, 3, 2, 1], insertion.reverseSort([8,5,2,7,8,9,3,8,6,3,8,1]));
+			assert.deepEqual([9, 8, 8, 8, 8, 7, 6, 5, 3, 3, 2, 1], insertion.reverseSort([8,5,2,7,8,8,3,1,6,3,8,9]));
 		})
 	})
 })
